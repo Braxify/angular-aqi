eb4fbf0ed84e0573514c20d5ebb910aa48486d81
Braxify <art.nazarian1@gmail.com>
2023-06-05 21:59:45 +0300

    add new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { TablesPageComponent } from './pages/tables-page/tables-page.component';
+import { MapPageComponent } from './pages/map-page/map-page.component';
+
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'tables', component: TablesPageComponent },
+  { path: 'map', component: MapPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GlobalErrorComponent } from './components/global-error/global-error.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { TablesPageComponent } from './pages/tables-page/tables-page.component';
+import { MapPageComponent } from './pages/map-page/map-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    GlobalErrorComponent,
+    NavigationComponent,
+    MainPageComponent,
+    TablesPageComponent,
+    MapPageComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
