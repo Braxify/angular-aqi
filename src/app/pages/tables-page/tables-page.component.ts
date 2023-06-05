9fb584c34188a600b8e59d5705e9895319ebb0f3
Braxify <art.nazarian1@gmail.com>
2023-06-05 23:45:15 +0300

    fixes

--- src/app/pages/tables-page/tables-page.component.ts
+++ src/app/pages/tables-page/tables-page.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tables-page',
   templateUrl: './tables-page.component.html',
-  styleUrls: ['./tables-page.component.scss']
+  styleUrls: ['./tables-page.component.scss'],
 })
-export class TablesPageComponent {
-
-}
+export class TablesPageComponent {}
