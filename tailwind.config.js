ae8a874033b65d67980fe4314c2dab35dcc4691d
Braxify <art.nazarian1@gmail.com>
2023-06-06 21:37:54 +0300

    update the sm breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '200px',
+      },
+    },
   },
   plugins: [],
 };
